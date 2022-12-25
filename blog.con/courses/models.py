3c1cbbd066e4f126808bbcb80836caa962d5228c
Dogukan karademir <b200109013@subu.edu.tr>
2022-12-25 20:58:55 +0300

    media oluşturuldu

--- blog.con/courses/models.py
+++ blog.con/courses/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Courses (models.Model):
-    name = models.CharField(max_length=200, unique=True , verbose_name="Kurs Adi",help_text="Kursun adini yaziniz")
+class Course(models.Model):
+    name = models.CharField(max_length=200, unique=True )
     description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to="courses/%Y%m/&d/", default="courses/icardi.jpg")   
+    image = models.ImageField(blank=True)   
     date = models.DateTimeField(auto_now=True)
     available = models.BooleanField(default=True)
     
+    def __str__(self):
+        return self.name
