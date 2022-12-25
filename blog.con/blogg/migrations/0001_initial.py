ce4a1a5546c314a6625a9b15c05b028346a44bb5
Dogukan karademir <b200109013@subu.edu.tr>
2022-12-25 22:40:38 +0300

    migration

---
+++ blog.con/blogg/migrations/0001_initial.py
@@ -0,0 +1,31 @@
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('product', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=50)),
+                ('description', models.CharField(blank=True, max_length=300)),
+                ('keywords', models.CharField(blank=True, max_length=200)),
+                ('image', models.ImageField(blank=True, upload_to='images/')),
+            
+                ('detail', models.TextField()),
+                ('status', models.CharField(choices=[('True', 'Evet'), ('False', 'Hayır')], max_length=10)),
+               
+                ('create_at', models.DateTimeField(auto_now_add=True)),
+                ('update_at', models.DateTimeField(auto_now=True)),
+                ('category', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='product.category')),
+                
+            ],
+        ),
+    ]
---
+++ blog.con/blogg/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Blog(models.Model):
+    
+    title = models.CharField(max_length=50)
+    description = models.CharField(blank=True, max_length=400)
+    keywords = models.CharField(blank=True, max_length=200)
+    image = models.ImageField(blank=True, upload_to='images/')
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True) 
+    detail = models.TextField()
+    def __str__(self):
+        return self.title
