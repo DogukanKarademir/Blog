ef96d98a4a225eb95d87917ef2b3aa6dda9fa46a
Dogukan karademir <b200109013@subu.edu.tr>
2022-12-26 09:33:19 +0300

    github hata verdi proje düzenlenip yeniden yapıldı

--- blog.con/blogg/models.py
+++ blog.con/blogg/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+class Category(models.Model):
+    title = models.CharField(max_length=50, null=True)
+
 class Blog(models.Model):
     STATUS = (
         ('True', 'Evet'),
